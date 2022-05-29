2904480cd643e7c91c0de702f0a1ea0d8696f22d
Yafira <yafira.martinez@lc.cuny.edu>
2022-05-28 20:53:00 -0400

    starting the engine with a class, types and constructor

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
 import { getObstacleEvents } from './computer-vision';
+
+interface AutonomousCarProps {
+  isRunning?: boolean
+
+}
+
+class Car {
+  isRunning = true;
+
+  constructor(props: AutonomousCarProps){
+    this.isRunning = props.isRunning;
+
+  }
+}
+
+const autonomousCar = new Car({ isRunning: true });
+console.log(autonomousCar.isRunning)
+
+interface AutonomousCar implements Car {
+  isRunning?: boolean;
+}
